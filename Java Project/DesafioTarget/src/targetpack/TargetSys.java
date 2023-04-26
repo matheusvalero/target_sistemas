bf70f7f3aaa1efba775b4cf9c223171e55731071
Matheus Valero <matheusvalero99@gmail.com>
2023-04-26 12:17:31 -0300

    Update Java

--- Java Project/DesafioTarget/src/targetpack/TargetSys.java
+++ Java Project/DesafioTarget/src/targetpack/TargetSys.java
@@ -5,6 +5,7 @@
 //Mesmo assim, aqui está o código em Java!...
 
 package targetpack;
+import java.util.Scanner;
 
 public class TargetSys {
     public static void main(String[] args) {
@@ -20,7 +21,17 @@ public class TargetSys {
         Exercicio ex4 = new Exercicio();
         ex4.quartoExercicio();
 
-        Exercicio ex5 = new Exercicio();
-        ex5.quintoExercicio();
+        Scanner esca = new Scanner(System.in);
+        System.out.print("Digite uma frase qualquer: ");
+        String frase = esca.nextLine();
+
+        String frase_invertida = "";
+        for ( int i = frase.length() -1 ; 0 <= i ; i++) {
+            frase_invertida += frase.charAt(i);
+        }
+
+        System.out.println("Assim fica a frase invertida: " + frase_invertida);
+        esca.close();
+
     }    
 }
